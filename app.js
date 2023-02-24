8739826a1b6423e95e9aee3c0043eed9c3ece61d
Mourya Pradeep Ramashare <mourya.18bcs1117@abes.ac.in>
2023-02-24 21:07:38 +0530

    route

--- app.js
+++ app.js
@@ -1,146 +1,72 @@
-// importing file system
-const fs=require('fs');
 // importing express
 
 const express =require('express');
+// using for morgon 
+const morgan=require('morgan');
+
+const tourRouter=require('./router/tourRouter');
+const userRouter=require('./router/userRouter');
+
 
 
 const app=express();
+
+
+app.use(morgan('dev'));
 
 // middleware for convert post data in the json format
 app.use(express.json());
 
 
 
-// importing data
-
-const tours=JSON.parse(
-    fs.readFileSync(`${__dirname}/data/simple.json`)
-)
-// for get api
-const getAllTours=(req,res)=>{
-    res.status(200).json({
-        status:'success',
-        results:tours.length,
-        data:{
-            tours
-        }
-    })
-
-}
 
 
-// for get api on the basis of the id
-const getTour=(req,res)=>{
-    console.log(req.params);
+// middleware
 
-    const id=req.params.id*1;
-// // instead of the below line use !tours
-//     if(id>tours.length){
-//         return res.status(404).json({
-//             status:'fail',
-//             message:"invalid Id"
-//         });
-//     }
+app.use((req,res,next)=>{
+    console.log("hello from middleware");
+    next();
+})
 
 
-    const tour_id=tours.find(el=>el.id===id);
-    if(!tour_id){
-        return res.status(404).json({
-            status:'fail',
-            message:"invalid Id"
-        });
-    }
-
-    res.status(200).json({
-        status:'success',
-        results:tours.length,
-        data:{
-            tour_id
-        }
-    })
-
-}
+// date middleware
 
 
-
-// for post api
-const createTour=(req,res)=>{
-    // console.log(req.body);
-    const newId=tours[tours.length-1].id+1;
-    const newTour=Object.assign({id:newId},req.body);
-
-    tours.push(newTour);
-    fs.writeFile(
-        `${__dirname}/data/simple.json`,
-        JSON.stringify(tours),
-        err=>{
-            res.status(201).json({
-                status:'success',
-                data:{
-                    tour:newTour
-                }
-            });
-        }
-    );
-    // not expected
-    // res.send("done");
-
-}
+app.use((req,res,next)=>{
+    res.requestTime=new Date().toISOString();
+    next();
+})
 
 
-const deleteTour=(req,res)=>{
-
-    if(req.params.id*1>tours.length){
-        return res.status(404).json({
-            status:'fail',
-            message:"invalid Id"
-        });
-    }
-
-
-    res.status(204).json({
-        status:'success',
-        data:null
-    })
-}
-
-
-// for delete options
-const updateTour=(req,res)=>{
-
-    const id=req.params.id*1;
-    if(id>tours.length){
-        return res.status(404).json({
-            status:'fail',
-            message:"invalid Id"
-        });
-    }
-
-
-    res.status(200).json({
-        status:'success',
-        data:{
-            tour:'updated tour'
-        }
-    })
-}
 // app.post('/api/v1/tours',createTour);
 // app.get('/api/v1/tours/:id',getTour)
 // app.get('/api/v1/tours',getAllTours)
 // app.patch('/api/v1/tours/:id',updateTour);
 // app.delete('/api/v1/tours/:id',deleteTour);
 
-app
-    .route('/api/v1/tours')
-    .get(getAllTours)
-    .post(createTour)
 
-app
-    .route('/api/v1/tours/:id')
-    .get(getTour)
-    .patch(updateTour)
-    .delete(deleteTour)
+
+
+
+
+// app.use('/api/v1/users',tourUser)
+
+// const tourRouter=express.Router();
+// const userRouter=express.Router();
+
+
+
+
+
+
+// for user route
+
+
+
+
+
+app.use('/api/v1/tours',tourRouter);
+app.use('/api/v1/user',userRouter);
 
 
 // server creation
