dcff564b88c2571f83b26675457389f4d7897502
Mourya Pradeep Ramashare <mourya.18bcs1117@abes.ac.in>
2023-03-02 10:17:42 +0530

    Crud started

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,31 +1,33 @@
 // importing file system
-const fs=require('fs');
+// const fs=require('fs');
 
+// importing data from the model
+const Tour=require('./../models/tourModel');
 
 
 
 // middleware
 
 
-// importing data
+// importing data in form of json
 
-const tours=JSON.parse(
-    fs.readFileSync(`${__dirname}/../data/simple.json`)
-)
+// const tours=JSON.parse(
+//     fs.readFileSync(`${__dirname}/../data/simple.json`)
+// )
 
+// for only middleware
 
+// exports.checkID=(req,res,next,val)=>{
+//     console.log(`Tour is ${val}`);
 
-exports.checkID=(req,res,next,val)=>{
-    console.log(`Tour is ${val}`);
-
-    if(req.params.id>tours.length){
-        return res.status(404).json({
-            status:'fail',
-            message:'invalid'
-        })
-    }
-    next();//it is important put next
-}
+//     if(req.params.id>tours.length){
+//         return res.status(404).json({
+//             status:'fail',
+//             message:'invalid'
+//         })
+//     }
+//     next();//it is important put next
+// }
 
 // first send request and then send the response
 exports.checkBody=(req,res,next)=>{
@@ -47,11 +49,11 @@ exports.getAllTours=(req,res)=>{
 
     res.status(200).json({
         status:'success',
-        results:tours.length,
-        requestAt:req.requestTime,
-        data:{
-            tours
-        }
+        // results:tours.length,
+        // requestAt:req.requestTime,
+        // data:{
+        //     tours
+        // }
     })
 
 }
@@ -64,21 +66,15 @@ exports.getTour=(req,res)=>{
     const id=req.params.id*1;
 
 
-    const tour_id=tours.find(el=>el.id===id);
-    // if(!tour_id){
-    //     return res.status(404).json({
-    //         status:'fail',
-    //         message:"invalid Id"
-    //     });
-    // }
+    // const tour_id=tours.find(el=>el.id===id);
 
-    res.status(200).json({
-        status:'success',
-        results:tours.length,
-        data:{
-            tour_id
-        }
-    })
+    // res.status(200).json({
+    //     status:'success',
+    //     results:tours.length,
+    //     data:{
+    //         tour_id
+    //     }
+    // })
 
 }
 
@@ -86,37 +82,36 @@ exports.getTour=(req,res)=>{
 
 // for post api
 exports.createTour=(req,res)=>{
-    // console.log(req.body);
-    const newId=tours[tours.length-1].id+1;
-    const newTour=Object.assign({id:newId},req.body);
-
-    tours.push(newTour);
-    fs.writeFile(
-        `${__dirname}/data/simple.json`,
-        JSON.stringify(tours),
-        err=>{
-            res.status(201).json({
-                status:'success',
-                data:{
-                    tour:newTour
-                }
-            });
+    res.status(201).json({
+        status:'success',
+        data:{
+            tour:newTour
         }
-    );
-    // not expected
-    // res.send("done");
+    });
+
+    // console.log(req.body);
+    // const newId=tours[tours.length-1].id+1;
+    // const newTour=Object.assign({id:newId},req.body);
+
+    // tours.push(newTour);
+    // fs.writeFile(
+    //     `${__dirname}/data/simple.json`,
+    //     JSON.stringify(tours),
+    //     err=>{
+    //         res.status(201).json({
+    //             status:'success',
+    //             data:{
+    //                 tour:newTour
+    //             }
+    //         });
+    //     }
+    // );
 
 }
 
 
 exports.deleteTour=(req,res)=>{
 
-    // if(req.params.id*1>tours.length){
-    //     return res.status(404).json({
-    //         status:'fail',
-    //         message:"invalid Id"
-    //     });
-    // }
 
 
     res.status(204).json({
