b6fd7b196828d98459890305b34faaaf84adc577
Mourya Pradeep Ramashare <mourya.18bcs1117@abes.ac.in>
2023-03-02 14:06:22 +0530

    module

---
+++ data/import-dev-data.js
@@ -0,0 +1,40 @@
+// importing mongoose package
+
+const mongoose=require('mongoose');
+
+
+
+const dotenv=require('dotenv');
+
+const app=require('./app');
+
+dotenv.config({path:'./config.env'});
+
+
+
+// Password for online
+
+
+// const DB=process.env.DATABASE.replace{
+//     '<PASSWORD>',
+//     process.env.DATABASE_PASSWORD
+// }
+// connection for mongoose package
+
+mongoose.connect(process.env.DATABASE_LOCAL,{
+    useNewUrlParser:true,
+    // useCreateIndex:true,
+    // useFindAndModify:false
+}).then(conn=>{
+    // console.log(conn.connections);
+    console.log("DB connected succesfully")
+})
+
+
+// creating the schema
+
+// server creation
+const port =process.env.PORT||3000;
+app.listen(port,()=>{
+    console.log(`App runing in the port ${port}`);
+})
