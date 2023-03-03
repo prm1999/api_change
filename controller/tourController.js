49ac3b490063a2f09c5de6ea53141096f3a9bca5
Mourya Pradeep Ramashare <mourya.18bcs1117@abes.ac.in>
2023-03-03 17:56:11 +0530

    reforting API

--- controller/tourController.js
+++ controller/tourController.js
@@ -1,9 +1,9 @@
 // importing file system
 // const fs=require('fs');
-
 // importing data from the model
 const Tour=require('./../models/tourModel');
-
+// Importing Utity Method
+const APIFeature =require('./../utils/apifeature')
 
 
     // middleware
@@ -14,73 +14,6 @@ const Tour=require('./../models/tourModel');
         next();
       };
       
-
-// API FEATURE FOR ALL SORTING AND SARCHING
-class APIFeature{
-    constructor(query,queryString){
-        this.query=query;
-        this.queryString=queryString;
-
-    }
-    filter(){
-        const queryObj={...this.queryString};
-        const excludeFiled=['page','sort','limit','fields'];
-        excludeFiled.forEach(ele=>delete queryObj[ele]);
-
-        // // Advance filtering
-        let queryStr = JSON.stringify(queryObj);
- 
-        queryStr = queryStr.replace(/\b(gte|te|lte|lt)\b/g, (match) => `$${match}`);
-        this.query= Tour.find(JSON.parse(queryStr));
-        // for returning the entire object
-        return this;
-
-        // console.log(JSON.parse(queryStr));
-
-
-    }
-    sort(){
-        if(this.queryString.sort){
-            const sortBy=this.queryString.sort.split(',').join(' ');
-            console.log(sortBy)
-            // sort first variable
-            this.query=this.query.sort(sortBy)
-        }
-        else{
-            // for removing v
-            this.query=this.query.select('-__v');
-        }
-        return this;
-
-    }
-
-    limitfilds(){
-        if(this.queryString.fields){
-            const fields=this.queryString.fields.split(',').join(' ');
-            this.query=this.query.select(fields);
-
-        }
-        else{
-            // for removing v
-            this.query=this.query.select('-__v');
-        }
-        return this;
-
-
-    }
-    pagginate(){
-        const page=this.queryString.page*1||1;  
-        const limit =this.queryString.limit*1||100;
-        const skip=(page-1)*limit;  
-        this.query=this.query.skip(skip).limit(limit);
-        // Return number of document
-        // if (this.query.page){
-        //     const numTour=await Tour.countDocuments();
-        //     if(skip>=numTour) throw new Error('This page does not exist');
-
-        return this;
-    }    
-}
 
     // for get api
 exports.getAllTours=async (req,res)=>{
