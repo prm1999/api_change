38f28f30faae0d04215ebeb9960842ae17386a2c
Mourya Pradeep Ramashare <mourya.18bcs1117@abes.ac.in>
2023-03-04 18:52:16 +0530

    global error

--- app.js
+++ app.js
@@ -48,13 +48,33 @@ app.use('/api/v1/users',userRouter);
 // * for all then make the middleware
 // original url is for the url that has been requested
 app.all('*',(req,res,next)=>{
-    res.status(404).json({
-        status:'fail',
-        message:`can't find ${req.originalUrl} on this server`
+    // res.status(404).json({
+    //     status:'fail',
+    //     message:`can't find ${req.originalUrl} on this server`
 
-    })
+    // })
+    // for unhandled route
+    const err=new Error(`can't find ${req.originalUrl} on this server`);
+    err.status='fail';
+    err.statusCode=404;
+    
+    // next will do the things which will apply that next function error
+    // next will be access the global handling error
+    next(err)
 })
 
+// use middleware for the handling the error
+app.use((err,req,res,next)=>{
+    // Internal server error
+    err.statusCode=err.statusCode||500;
+    err.status=err.status||'error';
+
+
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message
+    });
+})
 
 
 module.exports=app;
