e9ec6468b2983c4dc0fdd84484659b58d7db0b60
Mourya Pradeep Ramashare <mourya.18bcs1117@abes.ac.in>
2023-03-05 17:22:19 +0530

    witherror

--- controller/tourController.js
+++ controller/tourController.js
@@ -70,34 +70,41 @@ exports.getTour=async (req,res)=>{
 
 }
 
+// Adding the cathing error as per async function
+const catchAync=fn=>{
+    return(req,res,next) =>{
+        fn(res,res,next).catch(next);
+    };
+};
 
 
 // for post api
-exports.createTour=async (req,res)=>{
-    try{
+exports.createTour=catchAync(async (req,res,next)=>{
+    // Instead of new tour and then save we require the save the tour
+    const newTour=await  Tour.create(req.body);
+
+    res.status(201).json({
+        status:'success',
+        data:{
+            tour:newTour
+        }
+    });
+    // try{
 
             // const newTour=new Tour({});
     //  Tour.save()
-    // Instead of new tour and then save we require the save the tour
-   const newTour=await  Tour.create(req.body);
-
-   res.status(201).json({
-       status:'success',
-       data:{
-           tour:newTour
-       }
-   });
-
-    }
-    catch(err){
-        res.status(400).json({
-            status:'fail',
-            message:err
-        })
-    }
 
 
-}
+    // }
+    // catch(err){
+    //     res.status(400).json({
+    //         status:'fail',
+    //         message:err
+    //     })
+    // }
+
+
+    });
 
 
 exports.updateTour=async(req,res)=>{
