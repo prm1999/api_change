05e93bf6bbc81086b7779ce37c223a510f5151da
Mourya Pradeep Ramashare <mourya.18bcs1117@abes.ac.in>
2023-03-08 23:47:10 +0530

    jwt fixed

--- controller/authController.js
+++ controller/authController.js
@@ -1,15 +1,30 @@
-const User=require("./../models/userModel");
+const jwt=require('jsonwebtoken');
+const User=require('./../models/userModel');
+const catchAsync=require('./../utils/catchAsync')
 
 
-// Create new user
-exports.signup=async(req,res,next)=>{
+exports.signup=catchAsync( async(req,res,next)=>{
+    // const newUser = await User.create(req.body);
 
-        const newUser=await User.create(req.body);
-        res.status(201).json({
-            status:"success",
-            data:{
-                user:newUser
-            }
-        })
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+      });
 
-}
+    const token=jwt.sign({ id:newUser._id},"pradeep",{
+        expiresIn:process.env.JWT_EXPIRE_IN
+    });
+    console.log("XYU")
+    res.status(201).json({
+        status:'success',
+        token:token,
+        data:{
+            token:token,
+            user:newUser
+        }
+    });
+    
+});
+
