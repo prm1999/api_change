c29bf722a66706c863eeff355e3b7db20f9bb610
Mourya Pradeep Ramashare <mourya.18bcs1117@abes.ac.in>
2023-03-08 19:22:12 +0530

    user

--- models/userModel.js
+++ models/userModel.js
@@ -29,8 +29,25 @@ const UserSchema= new mongoose.Schema({
     passwordConfirm:{
         type:String,
         require:[true,"please confirm a passowrd"],
+        validate:{
+            validator:function(el){
+                // this only work as save
+                return el===this.password;
+            },
+            message:"Password are not a same"
+        }
+        
         
     }
+})
+// preschema on Save
+
+UserSchema.pre('save',function(next){
+    // if password is not modified
+    if (this.isModified('password')) return next();
+    // b script use for the encrpyt 
+
+
 })
 // for creating the user Schema
 const User=mongoose.model("user",UserSchema);
