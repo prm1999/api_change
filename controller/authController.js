620ddbc78a72a7c6afd692198a6a1dd1f60d784d
Mourya Pradeep Ramashare <mourya.18bcs1117@abes.ac.in>
2023-03-10 17:26:30 +0530

    send email to for reset password

--- controller/authController.js
+++ controller/authController.js
@@ -138,6 +138,34 @@ exports.forgotPassword=catchAsync(async(req,res,next)=>{
     // stop all the validator 
         await user.save({validateBeforeSave: false})
     // send it user 
-})
+
+    const resetURL = `${req.protocol}://${req.get(
+        'host'
+      )}/api/v1/users/resetPassword/${resetToken}`;
+    
+      const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
+    
+      try {
+        await sendEmail({
+          email: user.email,
+          subject: 'Your password reset token (valid for 10 min)',
+          message
+        });
+    
+        res.status(200).json({
+          status: 'success',
+          message: 'Token sent to email!'
+        });
+      } catch (err) {
+        user.passwordResetToken = undefined;
+        user.passwordResetExpires = undefined;
+        await user.save({ validateBeforeSave: false });
+    
+        return next(
+          new AppError('There was an error sending the email. Try again later!'),
+          500
+        );
+      }
+    });
 
 exports.resetPassword=(req,res,next)=>{}
